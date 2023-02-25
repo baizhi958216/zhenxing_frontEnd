c917684fc22605bb3cf4e0b78ca344c32880c7c1
baizhi958216 <1475289190@qq.com>
2023-02-25 09:50:22 +0800

    新增通用菜单组件
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,57 @@
+<template>
+  <view
+    class="h_wrap"
+    :style="{ height: `${height + top}px`, background: background }"
+  >
+    <view
+      class="h_wrap_items"
+      :style="{ top: `${top}px`, height: `${height}px` }"
+      v-if="has_item"
+    >
+      <view class="h_back">
+        <uni-icons type="back" size="25" color="white"></uni-icons>
+      </view>
+      <view class="h_title">
+        {{ title }}
+      </view>
+    </view>
+  </view>
+</template>
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    background?: string;
+    has_item?: boolean;
+  }>(),
+  {
+    title: "",
+    background: "",
+    has_item: false,
+  }
+);
+const { height, top } = uni.getMenuButtonBoundingClientRect();
+</script>
+<style scoped lang="scss">
+.h_wrap {
+  display: flex;
+  justify-content: center;
+  .h_wrap_items {
+    position: absolute;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+  }
+  .h_back {
+    background-color: rgba(109, 109, 109, 0.377);
+    padding: 5rpx;
+    border-radius: 50%;
+    margin-left: 20rpx;
+  }
+  .h_title {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+}
+</style>
