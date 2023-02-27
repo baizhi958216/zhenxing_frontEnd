25ee1d62336e99208636a54e31fa522ed9af407c
baizhi958216 <1475289190@qq.com>
2023-02-28 00:55:42 +0800

    初始化乡村振兴，乡村旅游, 乡村美食
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/subs/LvYou/LvYou.vue
@@ -0,0 +1,9 @@
+<template>
+  <header-view :has_item="true" has_back :back_background="false" />
+  <view :style="{ padding: `${top + height}px 40rpx 100rpx 40rpx` }"></view>
+</template>
+<script setup lang="ts">
+import HeaderView from "@/components/HeaderView.vue";
+const { height, top } = uni.getMenuButtonBoundingClientRect();
+</script>
+<style scoped></style>
---
+++ src/subs/MeiShi/MeiShi.vue
@@ -0,0 +1,14 @@
+<template>
+  <header-view
+    :has_item="true"
+    has_back
+    title="乡村美食"
+    :back_background="false"
+  />
+  <view :style="{ padding: `${top + height}px 40rpx 100rpx 40rpx` }"></view>
+</template>
+<script setup lang="ts">
+import HeaderView from "@/components/HeaderView.vue";
+const { height, top } = uni.getMenuButtonBoundingClientRect();
+</script>
+<style scoped></style>
