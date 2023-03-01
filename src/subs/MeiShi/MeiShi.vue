797e43e5f1ff87fd28b9bd7152e090d194734e2e
baizhi958216 <1475289190@qq.com>
2023-03-01 23:30:14 +0800

    乡村美食页基本骨架
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/subs/MeiShi/MeiShi.vue
+++ src/subs/MeiShi/MeiShi.vue
@@ -5,10 +5,108 @@
     title="乡村美食"
     :back_background="false"
   />
-  <view :style="{ padding: `${top + height}px 40rpx 100rpx 40rpx` }"></view>
+  <view :style="{ paddingTop: `${top + height}px ` }">
+    <!-- 推荐栏 -->
+    <view
+      :style="{
+        height: '150rpx',
+        backgroundImage: `url(${topRecommend.url})`,
+      }"
+      @click="toRecommend(topRecommend.sid)"
+    >
+    </view>
+    <!-- features -->
+    <view
+      style="
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20rpx;
+      "
+    >
+      <!-- 排序 -->
+      <view
+        style="
+          display: flex;
+          align-items: center;
+          justify-content: space-evenly;
+          color: #949494;
+          font-size: 22rpx;
+          width: 320rpx;
+        "
+      >
+        <view>综合排序</view>
+        <view>评分排序</view>
+        <view>价格排序</view>
+      </view>
+      <!-- 发现美食 -->
+      <view class="ms_seek">发现美食</view>
+    </view>
+    <!-- 列表 绑定排序菜单-->
+    <view class="ms_list">
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+      <view class="ms_list_item"></view>
+    </view>
+  </view>
 </template>
 <script setup lang="ts">
 import HeaderView from "@/components/HeaderView.vue";
+import { ref } from "vue";
 const { height, top } = uni.getMenuButtonBoundingClientRect();
+
+const topRecommend = ref({
+  sid: "2",
+  url: "https://tuchuangs.com/imgs/2022/05/26/5cd30fe855924bab.png",
+});
+
+const toRecommend = (sid: string) => {
+  console.log(sid);
+};
 </script>
-<style scoped></style>
+<style scoped>
+.ms_seek {
+  font-weight: bold;
+}
+.ms_seek::before {
+  content: "";
+  width: 120rpx;
+  height: 10rpx;
+  border-radius: 60rpx;
+  background-color: #ff673d;
+  position: absolute;
+  margin-top: 29rpx;
+  z-index: -100;
+}
+.ms_list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  overflow: scroll;
+  max-height: 75vh;
+}
+.ms_list_item {
+  background-color: green;
+  width: 300rpx;
+  height: 200rpx;
+  margin: 20rpx;
+}
+</style>
